c305cbbba5863591d99bf6bca3372f250dd3d309
Daniel St. Jules <danielst.jules@gmail.com>
2014-09-24 02:40:51 -0700

    Add callIn tests

--- test/dreyTest.js
+++ test/dreyTest.js
@@ -97,4 +97,83 @@ describe('drey', function() {
       });
     });
   });
+
+  describe('callIn', function() {
+    var count;
+
+    beforeEach(function() {
+      count = 0;
+
+      // Adding some nested methods
+      var fn = function() {
+        count++;
+      };
+
+      object.fn = fn;
+      object.foo.fn = fn;
+      object.foo.bar.fn = fn;
+    });
+
+    afterEach(function() {
+      delete object.fn;
+      delete object.foo.fn;
+      delete object.foo.bar.fn;
+    });
+
+    it('invokes the function at the given key if it exists', function() {
+      var keys, i, res;
+
+      keys = [
+        ['fn'],
+        ['foo', 'fn'],
+        ['foo', 'bar', 'fn']
+      ];
+
+      for (i = 0; i < keys.length; i++) {
+        res = drey.callIn(object, keys[i]);
+        expect(count).to.be(i + 1);
+        expect(res).to.be(object);
+      }
+    });
+
+    it('throws an exception if the key does not correspond to a fn', function() {
+      var fn = function() {
+        drey.callIn(object, ['foo', 'bar']);
+      };
+
+      var error = 'Could not find fn to call in object path: foo,bar';
+
+      expect(fn).to.throwException(function(err) {
+        expect(err.message).to.be(error);
+      });
+    });
+
+    it('accepts a variable number of arguments to pass to the fn', function() {
+      var keys, i, res, x;
+
+      object.foo.bar.fn = function(y, z) {
+        x = y + z;
+      };
+
+      res = drey.callIn(object, ['foo', 'bar', 'fn'], 10, 5);
+      expect(x).to.be(15);
+      expect(res).to.be(object);
+    });
+
+    it('accepts dot delimited properties for keys', function() {
+      var keys, i, res;
+
+      keys = [
+        'fn',
+        'foo.fn',
+        'foo.bar.fn'
+      ];
+
+      for (i = 0; i < keys.length; i++) {
+        res = drey.callIn(object, keys[i]);
+        expect(count).to.be(i + 1);
+        expect(res).to.be(object);
+      }
+    });
+  });
 });
