c28cb4a0147c37c8027f798bbd74c32046c73cab
Daniel St. Jules <danielst.jules@gmail.com>
2014-09-24 02:11:45 -0700

    Added tests for getIn

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # drey
 
 Nested property access for underscore and Lo-Dash. Also usable as a standalone
-library. Inspired by igorw/get-in.
+library, and available in the browser and node. Inspired by igorw/get-in.
 
 [![Build Status](https://travis-ci.org/danielstjules/drey.svg?branch=master)](https://travis-ci.org/danielstjules/drey)
 
--- test/dreyTest.js
+++ test/dreyTest.js
@@ -7,52 +7,93 @@ describe('drey', function() {
   // }
   var object = {foo: {bar: {baz: 'test'}}};
 
-  describe('isIn', function() {
-    describe('given an array of keys', function() {
-      it('returns true if the keys exist', function() {
-        var keys = [
-          ['foo'],
-          ['foo', 'bar'],
-          ['foo', 'bar', 'baz']
-        ];
+  describe('getIn', function() {
+    it('returns the value if the keys exist', function() {
+      var expected = [
+        {keys: ['foo'], res: object.foo},
+        {keys: ['foo', 'bar'], res: object.foo.bar},
+        {keys: ['foo', 'bar', 'baz'], res: object.foo.bar.baz}
+      ];
 
-        keys.forEach(function(array) {
-          var res = drey.isIn(object, array);
-          expect(res).to.be(true);
-        });
-      });
-
-      it('returns false if the keys do not exist', function() {
-        var keys = [
-          ['bar'],
-          ['foo', 'invalid'],
-          ['foo', 'bar', 'invalid']
-        ];
-
-        keys.forEach(function(array) {
-          var res = drey.isIn(object, array);
-          expect(res).to.be(false);
-        });
+      expected.forEach(function(exp) {
+        var res = drey.getIn(object, exp.keys);
+        expect(res).to.be(exp.res);
       });
     });
 
-    describe('given a string of keys', function() {
-      it('returns true if the keys exist', function() {
-        var keys = ['foo', 'foo.bar', 'foo.bar.baz'];
+    it('returns undefined, by default, if the keys do not exist', function() {
+      var keys = [
+        ['bar'],
+        ['foo', 'invalid'],
+        ['foo', 'bar', 'invalid']
+      ];
 
-        keys.forEach(function(array) {
-          var res = drey.isIn(object, array);
-          expect(res).to.be(true);
-        });
+      keys.forEach(function(array) {
+        var res = drey.getIn(object, array);
+        expect(res).to.be(undefined);
       });
+    });
 
-      it('returns false if the keys do not exist', function() {
-        var keys = ['bar', 'foo.invalid', 'foo.bar.invalid'];
+    it('returns the given defaultVal when the keys do not exist', function() {
+      var keys = [
+        ['bar'],
+        ['foo', 'invalid'],
+        ['foo', 'bar', 'invalid']
+      ];
 
-        keys.forEach(function(array) {
-          var res = drey.isIn(object, array);
-          expect(res).to.be(false);
-        });
+      keys.forEach(function(array) {
+        var res = drey.getIn(object, array, 'test');
+        expect(res).to.be('test');
+      });
+    });
+
+    it('accepts dot delimited properties for keys', function() {
+      var expected = [
+        {keys: 'foo', res: object.foo},
+        {keys: 'foo.bar', res: object.foo.bar},
+        {keys: 'foo.bar.baz', res: object.foo.bar.baz}
+      ];
+
+      expected.forEach(function(exp) {
+        var res = drey.getIn(object, exp.keys);
+        expect(res).to.be(exp.res);
+      });
+    });
+  });
+
+  describe('isIn', function() {
+    it('returns true if the keys exist', function() {
+      var keys = [
+        ['foo'],
+        ['foo', 'bar'],
+        ['foo', 'bar', 'baz']
+      ];
+
+      keys.forEach(function(array) {
+        var res = drey.isIn(object, array);
+        expect(res).to.be(true);
+      });
+    });
+
+    it('returns false if the keys do not exist', function() {
+      var keys = [
+        ['bar'],
+        ['foo', 'invalid'],
+        ['foo', 'bar', 'invalid']
+      ];
+
+      keys.forEach(function(array) {
+        var res = drey.isIn(object, array);
+        expect(res).to.be(false);
+      });
+    });
+
+    it('accepts dot delimited properties for keys', function() {
+      var keys = ['foo', 'foo.bar', 'foo.bar.baz'];
+
+      keys.forEach(function(array) {
+        var res = drey.isIn(object, array);
+        expect(res).to.be(true);
       });
     });
   });
