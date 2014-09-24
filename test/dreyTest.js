d61563dc64c018077c7bcb78fd6ef4ccb7ed424a
Daniel St. Jules <danielst.jules@gmail.com>
2014-09-24 00:55:40 -0700

    Basic lib code

---
+++ test/dreyTest.js
@@ -0,0 +1,10 @@
+var expect = require('expect.js');
+var drey   = require('../lib/drey.js');
+
+describe('drey', function() {
+  // if (global.mocha && ieDetected) {
+  //   global.mocha.globals(['CrossStorageClient-*']);
+  // }
+
+
+});
